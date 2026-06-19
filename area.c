60ee3190f4706b211f42649f6c19c19717276249
Anushka Shettigar <anushka.shettigar.1706@gmail.com>
2026-06-20 00:32:51 +0530

    day one

---
+++ area.c
@@ -0,0 +1,11 @@
+// write a progam to find the area of a square
+#include<stdio.h>
+int main(){
+    int a, s;
+    printf("enter the side of the square");
+    scanf("%d", &s);
+    a = s*s;
+    printf("the area of the square is %d", a);
+    return 0;
+
+}
