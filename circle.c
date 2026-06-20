f1751d84997bd22b023a7361804cd882b87a2555
Anushka Shettigar <anushka.shettigar.1706@gmail.com>
2026-06-20 20:33:54 +0530

    day two

---
+++ circle.c
@@ -0,0 +1,13 @@
+//area and perimeter of the circle
+#include<stdio.h>
+int main(){
+    float r, p, a;
+    const float pi = 3.14;
+    printf("enter the radius of the circle");
+    scanf("%f", &r);
+    p = 2*pi*r;
+    a = pi*r*r;
+    printf("the perimeter of the circle is %.2f\n the area of the circle is %.2f", p, a);
+    return 0;
+
+}
